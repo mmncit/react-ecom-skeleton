ea71b6768e6d8fbf4eed5d9ac9bbdc79e2824bc7
mamun <mamun.rashid@seequent.com>
2021-11-08 20:53:48 -0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  React.createElement("h1", null, "Hello, World!"),
+  document.getElementById("root")
+);
