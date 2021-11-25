df137af2b732a6959bcbbc168d3b55919272fab4
mamun <mamun.rashid@seequent.com>
2021-11-24 22:52:04 -0700

    create react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>Mohabbatie's Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  React.createElement(
-    "ul",
-    { style: { color: "blue" } },
-    React.createElement("li", { style: { color: "green" } }, "Monday"),
-    React.createElement("li", { style: { color: "red" } }, "Tuesday")
-  ),
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
